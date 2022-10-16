a331073be15daf80f62b32258de5c7024a691526
randreu28 <randreu28@gmail.com>
2022-10-16 23:06:18 +0200

    Adds loading status

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 import Shader from "./Shader";
 
 export default function App() {
   return (
     <>
       <div className="h-screen w-screen -z-10 fixed">
-        <Canvas>
-          <Shader />
-        </Canvas>
+        <Suspense
+          fallback={<span className="m-auto text-5xl">Loading...</span>}
+        >
+          <Canvas>
+            <Shader />
+          </Canvas>
+        </Suspense>
       </div>
     </>
   );
