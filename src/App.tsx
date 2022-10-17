0ae6530194208799926f938e55bb8bb3fbb75dd3
randreu28 <randreu28@gmail.com>
2022-10-17 15:27:27 +0200

    Creates a loading UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Shader from "./Shader";
+import Loading from "./Loading";
 
 export default function App() {
   return (
     <>
-      <div className="h-screen w-screen -z-10 fixed">
-        <Suspense
-          fallback={<span className="m-auto text-5xl">Loading...</span>}
-        >
+      <Suspense fallback={<Loading />}>
+        <div className="h-screen w-screen -z-10 fixed">
           <Canvas>
             <Shader />
           </Canvas>
-        </Suspense>
-      </div>
+        </div>
+      </Suspense>
     </>
   );
 }
